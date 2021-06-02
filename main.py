36f5d227c6e9c722f423c8fb934b6c679c3ce488
Sebb <sebb@apache.org>
2021-06-03 00:01:11 +0100

    Flush

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import subprocess
 if __name__ == '__main__':
     PYTHON3 = sys.executable
     cliargs = [PYTHON3, 'sub.py']
-    print("Before call", file=sys.stderr)
+    print("Before call", file=sys.stderr, flush=True)
     rv = subprocess.check_output(cliargs)
-    print("After call", file=sys.stderr)
+    print("After call", file=sys.stderr, flush=True)
     print("Summary")
     print("1")
     print("2")
